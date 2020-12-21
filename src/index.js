8d8fa0983870bb688cdd1ca0d334a807494163df
Caue13087 <caue13087@gmail.com>
2020-12-20 23:17:16 -0200

    tarefa quase pronta

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Login from './pages/login';
+import Cadastrar from './pages/cadastrar';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {FirebaseAppProvider} from 'reactfire';
+import firebaseConfig from './utils/firebaseConfig';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Cadastrar />
+    </FirebaseAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBqRqtvITsBSgSBYs8c4Hbgg6ezSCkX0dw",
+    authDomain: "recuperacao-senai.firebaseapp.com",
+    projectId: "recuperacao-senai",
+    storageBucket: "recuperacao-senai.appspot.com",
+    messagingSenderId: "809183383597",
+    appId: "1:809183383597:web:8ccb31c5a04e42893de8a4"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebaseConfig;
